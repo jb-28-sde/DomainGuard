38ffd8868c984780adc8c2898549ca3365a88728
jaibhim bangrey <jaibhimb@gmail.com>
2026-03-27 20:46:58 +0530

    fix controllers and clean imports

--- Backend/Routes/ScanRoutes.js
+++ Backend/Routes/ScanRoutes.js
@@ -1,20 +1,8 @@
 import express from "express";
-import { createScan } from "../Controllers/ScanController.js";
-import generateVariants from "../Domain-analysis/DomainvariantGenerator.js";
+import { createScan, generateDomainVariants } from "../Controllers/ScanController.js";
 
 const router = express.Router();
 
 router.post("/scan", createScan);
-router.post("/generate", (req, res) => {
-  const { domain } = req.body;
-  if (!domain) {
-    return res.status(400).json({ message: "Domain is required" });
-  }
-  const variants = generateVariants(domain);
-  res.json({
-    original: domain,
-    total: variants.length,
-    variants,
-  });
-});
+router.post("/generate", generateDomainVariants)  
 export default router;
--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import scanRoutes from "./Routes/ScanRoutes.js";
-import generateVariants from "./Domain-analysis/DomainvariantGenerator.js";
 dotenv.config();
 const app = express();
 
