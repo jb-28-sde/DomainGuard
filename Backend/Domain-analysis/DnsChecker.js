b2b4b027e83bb6f0c1564e6196a43ca3261c0067
Nandini Pendyam <nandu.pendyam@gmail.com>
2026-03-30 21:18:00 +0530

    updated DNS checker

--- Backend/Domain-analysis/DnsChecker.js
+++ Backend/Domain-analysis/DnsChecker.js
@@ -5,8 +5,8 @@ import dns from "dns/promises";
 // Function to check if a domain exists
 export async function checkDNS(domain) {
   try {
-    const addresses = await dns.resolve(domain, "A"); // IPv4 check
-    return addresses.length > 0; // Returns true if at least one IP found
+    const addresses = await dns.lookup(domain); // IPv4 check
+    return addresses ? true : false; // Returns true if at least one IP found
   } catch (error) {
     return false; // Returns false if domain is invalid or DNS lookup fails
   }
