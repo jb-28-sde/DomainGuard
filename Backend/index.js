ef68770f3f6b1f0d7e9cf3cec01f6bd5f950a895
jaibhim bangrey <jaibhimb@gmail.com>
2026-05-02 16:11:23 +0530

    frontend API URL updated for production + worker merged into backend

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,7 @@ import logger from "./Middlewares/Logger.js";
 import scanRoutes from "./Routes/ScanRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import "./worker.js";
 
 // __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
